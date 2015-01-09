5ff3a8e330ebb5cfd1c95fc9791b008ee9efaef6
James Yanyuk <jyanyuk@umass.edu>
2015-01-09 18:06:30 -0500

    Finished basic login/signup views and models, now to setup db

--- client/app/scripts/controllers/login.js
+++ client/app/scripts/controllers/login.js
@@ -8,5 +8,25 @@
  * Controller of the clientApp
  */
 angular.module('apollonApp')
-  .controller('LoginCtrl', function ($scope) {
+  .controller('LoginCtrl', function ($scope, $http) {
+    $scope.login = {};
+    $scope.login.user = {};
+    $scope.message = '';
+
+    $scope.login.submit = function() {
+      if(!$scope.login.user.username || !$scope.login.user.password) {
+        $scope.message = 'Field(s) left blank.';
+        return false;
+      }
+
+      console.log($scope.signup.user);
+
+      $http.post('/login', $scope.login.user)
+        .success(function(data) {
+          console.log('Login success.');
+        })
+        .error(function(data) {
+          console.log('Login failure.');
+        });
+    }
   });
--- client/app/scripts/controllers/signup.js
+++ client/app/scripts/controllers/signup.js
@@ -8,5 +8,25 @@
  * Controller of the clientApp
  */
 angular.module('apollonApp')
-  .controller('SignupCtrl', function ($scope) {
+  .controller('SignupCtrl', function ($scope, $http) {
+    $scope.signup = {};
+    $scope.signup.user = {};
+    $scope.message = '';
+
+    $scope.signup.submit = function() {
+      if(!$scope.signup.user.username || !$scope.signup.user.password) {
+        $scope.message = 'Field(s) left blank.';
+        return false;
+      }
+
+      console.log($scope.signup.user);
+
+      $http.post('/signup', $scope.signup.user)
+        .success(function(data) {
+          console.log('Signup success.');
+        })
+        .error(function(data) {
+          console.log('Signup failure.');
+        });
+    }
   });
