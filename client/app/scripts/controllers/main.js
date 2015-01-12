b03d23dcc79ea0dbd9be79d2606daac1f94cf380
Steven Pham <tungpham31@gmail.com>
2015-01-12 01:32:36 -0500

    Create an authentication service

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -8,13 +8,13 @@
  * Controller of the clientApp
  */
 angular.module('apollonApp')
-  .factory('socket', function(socketFactory) {
+  .factory('socket', function (socketFactory) {
     return socketFactory();
   })
   .controller('MainCtrl', ['$scope', 'socket', function ($scope, socket) {
     $scope.chat = {}
-    $scope.chat.send = function() {
-      if($scope.chat.message) {
+    $scope.chat.send = function () {
+      if ($scope.chat.message) {
         socket.emit('sendmessage', {
           // todo
           message: $scope.chat.message
@@ -23,21 +23,21 @@ angular.module('apollonApp')
       }
     }
 
-    socket.on('connect', function(data) {
+    socket.on('connect', function (data) {
       socket.emit('join_radio', {
         // todo: transmit user nickname
       });
     });
 
-    socket.on('updatechat', function(data) {
+    socket.on('updatechat', function (data) {
       // todo: add received message
     });
 
-    socket.on('client_connect', function(data) {
+    socket.on('client_connect', function (data) {
       // todo: add c message
     });
 
-    socket.on('client_disconnect', function(data) {
+    socket.on('client_disconnect', function (data) {
       // todo: add dc message
     });
   }]);
