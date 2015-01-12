e31019987d341ab61479f8b95eec25d43e8c6722
Steven Pham <tungpham31@gmail.com>
2015-01-12 02:22:57 -0500

    Neats

--- server/lib/socket/index.js
+++ server/lib/socket/index.js
@@ -1,39 +1,39 @@
 var idmap = [];
 
-module.exports = function(io) {
-    io.sockets.on('connection', function(socket) {
-        console.log('\nTEST\n');
+module.exports = function (io) {
+	io.sockets.on('connection', function (socket) {
+		console.log('\nTEST\n');
 
-        socket.on('join_radio', function(data) {
-            socket.join('radio_' + data.radioid);
-            console.log(data.nickname + ' joined ' + data.radioid + '.');
+		socket.on('join_radio', function (data) {
+			socket.join('radio_' + data.radioId);
+			console.log(data.nickname + ' joined ' + data.radioId + '.');
 
-            io.sockets.in('radio_' + data.radioid).emit('updatechat', {
-                message: {
-                    sender: '[SERVER]',
-                    body: data.nickname + ' is now listening.'
-                }
-            });
-        });
+			io.sockets.in('radio_' + data.radioId).emit('updatechat', {
+				message: {
+					sender: '[SERVER]',
+					body: data.nickname + ' is now listening.'
+				}
+			});
+		});
 
-        socket.on('sendmessage', function(data) {
-            io.sockets.in('radio_' + data.radioid).emit('updatechat', {
-                message: {
-                    sender: data.nickname,
-                    body: data.message
-                }
-            });
-        });
+		socket.on('sendmessage', function (data) {
+			io.sockets.in('radio_' + data.radioId).emit('updatechat', {
+				message: {
+					sender: data.nickname,
+					body: data.message
+				}
+			});
+		});
 
-        socket.on('disconnect', function(data) {
-            console.log('Socket: ' + socket.id + ' disconnected from server.');
+		socket.on('disconnect', function (data) {
+			console.log('Socket: ' + socket.id + ' disconnected from server.');
 
-            io.sockets.in('radio_' + data.radioid).emit('updatechat', {
-                message: {
-                    sender: '[SERVER]',
-                    body: data.nickname + ' has left.'
-                }
-            });
-        });
-    });
+			io.sockets.in('radio_' + data.radioId).emit('updatechat', {
+				message: {
+					sender: '[SERVER]',
+					body: data.nickname + ' has left.'
+				}
+			});
+		});
+	});
 }
