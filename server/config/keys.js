7c7302392ad02f4f04c19b1e8579f37398970014
Thai Nguyen <thainguyen@umass.edu>
2015-01-21 01:16:43 -0500

    add keys for test app

--- server/config/keys.js
+++ server/config/keys.js
@@ -3,9 +3,17 @@ module.exports = {
 
   youtube: 'AIzaSyA7QOe5_6VAQBnO-XihFvcBOV1xomJ1gaQ',
 
+  //test app for localhost:3000
   facebook: {
-    clientID: '1559394300974294',
-    clientSecret: '50ff897b9bcece9393c233939ebc0e36',
+    clientID: '1561796027400788',
+    clientSecret: 'ff5641da40e3bd1e352e7947c9a7a9f7',
     callbackURL: '/auth/facebook/callback'
   }
+  
+  //main app for blink.fm
+  // facebook: {
+  //   clientID: '1559394300974294',
+  //   clientSecret: '50ff897b9bcece9393c233939ebc0e36',
+  //   callbackURL: '/auth/facebook/callback'
+  // }
 };
