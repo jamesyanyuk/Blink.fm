e16b2f91599bb6658705934581a151a6c308dcf8
James Yanyuk <jyanyuk@umass.edu>
2015-02-06 00:59:49 -0500

    Secured player status on server side

--- server/lib/socket.js
+++ server/lib/socket.js
@@ -149,10 +149,14 @@ module.exports = function(io) {
 		});
 
 		socket.on('broadcast_player_status', function(data) {
-			io.sockets.in('radio_' + data.radioId).emit('update_player_status', data);
+			// Temporary server-side player update authentication
+			if (radioMap[data.radioId] && (radioMap[data.radioId].socketid === socket.id))
+				io.sockets.in('radio_' + data.radioId).emit('update_player_status', data);
 		});
 
 		socket.on('send_message', function(data) {
+			console.log(radioMap);
+			console.log(socketMap);
 			io.sockets.in('radio_' + data.radioid).emit('update_chat', {
 				message: {
 					sender: data.nickname,
