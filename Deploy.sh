e3393c11e9e076244ee196737ba980b50ddb657a
James Yanyuk <jyanyuk@umass.edu>
2015-02-23 22:41:39 -0500

    Creating a more streamlined deployment procedure

--- Deploy.sh
+++ Deploy.sh
@@ -1,8 +1,8 @@
 git submodule add -f https://github.com/tungpham31/Apollon.git deployment_environment
 cd deployment_environment
-git checkout production
 git submodule init
 git submodule update --force
+git checkout production
 cd client
 rm -f .gitignore
 npm install
